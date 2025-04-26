caec511dc8fd80cd48cb5f84d8425603f55b3cb5
sohyun-jang <ellajang@inha.edu>
2025-04-27 00:58:01 +0900

    python-base-programming W3

---
+++ Python-based-programming/W3-Q1-solution.py
@@ -0,0 +1,7 @@
+st = input("문자열을 입력하세요: ")
+
+print("문자열의 길이: {}".format(len(st)))
+print("대문자로 변환: {}".format(st.upper()))
+print("소문자로 변환: {}".format(st.lower()))
+print("앞뒤 공백 제거: {}".format(st.strip()))
+print("단어 분리: {}".format(st.split()))
